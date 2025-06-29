50b2b9a04e277fb364b347af64c1973604836b10
Sophia Cheng <skcheng@umich.edu>
2025-06-28 20:50:24 -0400

    Add mehtod for resources path

--- src/signals_in_the_noise/utilities/storage.py
+++ src/signals_in_the_noise/utilities/storage.py
@@ -2,9 +2,16 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 DATA_DIRECTORY = PROJECT_ROOT / "data"
+RESOURCES_DIRECTORY = PROJECT_ROOT / "resources"
 
 def get_data_path(file_or_directory: str=None):
     if file_or_directory is None:
         return DATA_DIRECTORY
     else:
         return DATA_DIRECTORY / file_or_directory
+
+def get_resources_path(file_or_directory: str=None):
+    if file_or_directory is None:
+        return RESOURCES_DIRECTORY
+    else:
+        return RESOURCES_DIRECTORY / file_or_directory
